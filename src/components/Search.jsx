34b0c01f8c375e774828842d397d0960345ef4a7
Nghia Nguyen <aihgn224@gmail.com>
2023-01-20 15:39:16 -0800

    added function for search feature

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -2,11 +2,31 @@ import React from 'react';
 import styled from 'styled-components';
 import {useState} from 'react';
 import { FaSearch } from 'react-icons/fa';
+import { useNavigate } from 'react-router-dom';
 
 function Search() {
+    const[input, setInput] = useState('');
+    const navigate = useNavigate();
+
+
+    const submitHandler = (e) => {
+        e.preventDefault();
+        navigate('/searched/' + input);
+
+
+    };
+
+
   return (
-    <FormStyle>
-        <input type='text' />
+    <FormStyle onSubmit={submitHandler}>
+        <div>
+        <FaSearch></FaSearch>
+        <input onChange={(e) =>setInput(e.target.value)}  
+        type='text' 
+        value={input}
+        />
+       
+        </div>
     </FormStyle>
   )
 }
@@ -14,7 +34,7 @@ function Search() {
 const FormStyle = styled.form `
     margin: 0rem 20rem;
     position: relative;
-    width: 100%;
+    
     input{
         border:none;
         background: linear-gradient(35deg, #494949, #313131);
@@ -23,6 +43,7 @@ const FormStyle = styled.form `
         border: none;
         border-radius: 1rem;
         outline: none;
+        width: 100%;
     }
     svg{
         position:absolute;
