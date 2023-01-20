5e8e792f675216570f2e4faab491a7861ce7107e
Nghia Nguyen <aihgn224@gmail.com>
2023-01-20 15:42:48 -0800

    added recipe to pages

---
+++ src/pages/Recipe.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { useEffect, useState } from 'react';
+import styled from 'styled-components';
+import { useParams } from 'react-router-dom';
+
+function Recipe() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Recipe
