a1ddfc7608f9e89f8559b3925e780eba9fc9ace3
Nghia Nguyen <aihgn224@gmail.com>
2023-02-04 15:17:56 -0800

    added instructions and ingredients

--- src/components/Veggie.jsx
+++ src/components/Veggie.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import styled from "styled-components";
 import { Splide, SplideSlide } from '@splidejs/react-splide';
 import '@splidejs/react-splide/css';
+import { Link } from "react-router-dom";
 
 function Veggie() {
   const [veggie, setVeggie] = useState([]);
@@ -45,8 +46,10 @@ function Veggie() {
                return(
                    <SplideSlide key={recipe.id}>
                         <Card>
+                            <Link to={'/recipe/' + recipe.id}>
                        <p> {recipe.title} </p>
                        <img src= {recipe.image} alt={recipe.title} />
+                            </Link>
                    </Card>
                    </SplideSlide>
                );
