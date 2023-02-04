5e28eed7c00fa60e323596298855dd6953142c37
Nghia Nguyen <aihgn224@gmail.com>
2023-02-04 15:29:51 -0800

    added details in instructions and ingredients

--- src/pages/Recipe.jsx
+++ src/pages/Recipe.jsx
@@ -34,10 +34,19 @@ function Recipe() {
         <Button className={activeTab === 'ingredients' ? 'active' : ''}  onClick={() => setActiveTab('ingredients')}>
           Ingredients
           </Button>
-          <div>
+            {activeTab === 'instructions' && (
+            <div>
             <h3 dangerouslySetInnerHTML={{__html:details.summary}}></h3>
             <h3 dangerouslySetInnerHTML={{__html:details.instructions}} ></h3>
           </div>
+          )};
+          {activeTab === 'ingredients' && (<ul>
+            {details.extendedIngredients.map((ingredient) => (
+              <li key={ingredient.id}>{ingredient.original}</li>
+            ))}
+            
+          </ul>)}
+          
       </Info>
     </DetailWrapper>
   )
