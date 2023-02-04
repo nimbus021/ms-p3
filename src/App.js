7084b1ec58663eae6d7417bb2befc39880db3184
Nghia Nguyen <aihgn224@gmail.com>
2023-02-04 15:54:19 -0800

    added a name

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Search from "./components/Search";
 function App() {
   return (
     <div className="App">
-      <h1>Recipe App</h1>
+      <h1>Nothing To Something</h1>
       <BrowserRouter>
       <Search />
         <Category />
